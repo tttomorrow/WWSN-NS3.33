630c24337573a1fbbc6237a0c58bc8eaa7cb7bd5
wcl <wangcl929@hust.edu.cn>
2024-11-01 19:25:40 +0800

    add model

---
+++ src/soilMoistureUpdater/helper/soilMoistureUpdater-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef SOILMOISTUREUPDATER_HELPER_H
+#define SOILMOISTUREUPDATER_HELPER_H
+
+#include "ns3/soilMoistureUpdater.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* SOILMOISTUREUPDATER_HELPER_H */
+
