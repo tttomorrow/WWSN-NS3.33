b1540f120a5a8d2ce6ca48c70eb401e7c0c33b5e
wcl <wangcl929@hust.edu.cn>
2024-11-21 17:01:31 +0800

    1

--- src/AODVwithBHandSF/helper/AODVwithBHandSF-helper.cc
+++ src/AODVwithBHandSF/helper/AODVwithBHandSF-helper.cc
@@ -72,7 +72,7 @@ AodvBHSFHelper::SetMaliciousNodes(NodeContainer nodes, double blackholeRatio, do
   std::unordered_set<int> uniqueNumbers;
   const uint32_t fixedSeed = 12345;  // 随机数种子
   std::mt19937 gen(fixedSeed);  // 使用 Mersenne Twister 算法
-  std::uniform_int_distribution<> dis(1, nodes.GetN()); // 定义随机数分布
+  std::uniform_int_distribution<> dis(2, nodes.GetN()); // 定义随机数分布
   uint32_t count = ((blackholeRatio + selectiveForwardingRatio) * nodes.GetN());
   uint32_t randomNumbers[count];
   NS_LOG_DEBUG("malicious Nodes number" << count << " .");
